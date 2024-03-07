421ef1ef1a9363028d974131854fe142a90894c6
Sougata Bhowmick <91432776+Sougata02@users.noreply.github.com>
2024-03-08 01:16:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,26 @@ const router = require('./routes/router');
 require('dotenv').config();
 
 const app = express();
+
+// Configure session
 app.use(session({
     secret: 'nimai'
 }));
-// Access to fetch at 'https://billing-app-iota.vercel.app/getproduct/all' from origin 'https://billing-app-front.vercel.app' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
-app.use(cors(
-    {
-        // origin:'https://billing-app-front.vercel.app',
-        origin:[*],
-        methods:['POST','GET'],
-        credentials:true
-    }
-    ))
+
+// Enable CORS middleware
+app.use(cors({
+    origin: 'https://billing-app-front.vercel.app', // Change this to your front-end URL
+    methods: ['POST', 'GET'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(router);
-app.listen(process.env.PORT_NO || 4000,()=>{
+
+// Start server
+app.listen(process.env.PORT_NO || 4000, () => {
     console.log("Server Started!!");
-})
+});
+
+// Connect to database
 dbConnect();
